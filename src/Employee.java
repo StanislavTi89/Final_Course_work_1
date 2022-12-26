05a8d8c4041215730a54131d50e1366a55a7dcd1
StanislavTi89 <Timoshenko311989@gmail.com>
2022-12-26 13:59:33 +0300

    Курсовая_работа_1

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 
 public class Employee {
     private String name;
@@ -18,8 +19,15 @@ public class Employee {
     }
 
     public String toString() {
-        return "Сотрудник: " + "Ф.И.О. " + surName + " " + name + " " +lastName+ ", " + id + "Сотрудник отдела: " + department + ", выплата оклада: " + salary;
+        return "Сотрудник: " + "Ф.И.О. " + surName + " " + name + " " +lastName+ ", " + id + "Сотрудник отдела: " + department + ", выплата оклада: " + String.format("%.2f",salary);
     }
+    //String sf1=String.format("name is %s",name);
+    //String sf2=String.format("value is %f",32.33434);
+    //String sf3=String.format("value is %32.12f",32.33434);
+    // returns 12 char fractional part filling with 0
+    //name is Stan
+    //value is 32.334340
+    //value is 32.334340000000
 
     public boolean isDepartment(String department) {
         if (department.equals("1") || department.equals("2") || department.equals("3") || department.equals("4") || department.equals("5")) {
@@ -35,5 +43,24 @@ public class Employee {
         Employee employee = (Employee) obj;
         return id == employee.id;
     }
+
+    //public boolean equals(Object o) {
+    //        // now check the reference of both object
+    //        if(this == o) return true;
+    //        // check the type of class
+    //        if(o == null || o.getClass() != this.getClass()) return false;
+    //        // now compare the value
+    //        Employee employee = (Employee)o;
+    //        if(employee.id == this.id && employee.name.equals(this.name)) {
+    //            return true;
+    //        } else return false;
+    //    }
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+    //public int hashCode() {
+    // here we are using id. We can also use other logic such as prime number addition or memory address.
+    //  return ...id;
+    //    }
 
 }
