d3671acfb56f815257fdeaf2e2462e217122667f
StanislavTi89 <Timoshenko311989@gmail.com>
2022-12-26 10:15:59 +0300

    Курсовая_работа_1

--- src/Employee.java
+++ src/Employee.java
@@ -5,6 +5,16 @@ public class Employee {
     public String lastName;
     public int department;
     public double salary;
+    public long id = count;
+    public static long count = 1L;
 
+    public Employee(String surName, String name, String lastName, int department, double salary) {
+        this.surName = surName;
+        this.name = name;
+        this.lastName = lastName;
+        this.department = department;
+        this.salary = salary;
+
+    }
 
 }
